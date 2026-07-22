c7ad4903731842b8a04d9f9f41ae21d3e4fd5a31
ramezani1367 <ramezani1367@gmail.com>
2026-07-22 10:12:05 +0330

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "ir.ramezani.expensenotebook"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "ir.ramezani.expensenotebook"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
